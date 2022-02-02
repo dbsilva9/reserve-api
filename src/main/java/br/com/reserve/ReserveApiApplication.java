0a916b8945c8466f298f0e37909ed2256c244f25
Diego Boaventura <silva.diego@mj.gov.br>
2022-02-02 16:00:02 -0300

    Adição das bibliotecas no projeto e estrutura do spring boot

---
+++ src/main/java/br/com/reserve/ReserveApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveApiApplication.class, args);
+	}
+
+}
